f7d0b5159f72bd7c9fa2e4fbbf4edcf3b74736fa
kavimgupta <135196394+kavimgupta@users.noreply.github.com>
2024-06-12 14:23:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [totalSpend, setTotalSpend] = useState('');
+    const [maxVisits, setMaxVisits] = useState('');
+    const [notVisitedInMonths, setNotVisitedInMonths] = useState('');
+    const [customers, setCustomers] = useState([]);
+
+    const handleFilter = async () => {
+        const response = await axios.post('http://localhost:5000/api/customers/filter', { 
+            total_spend: totalSpend,
+            max_visits: maxVisits,
+            not_visited_in_months: notVisitedInMonths,
+        });
+        setCustomers(response.data);
+    };
+
+    return (
+        <div>
+            <h1>Customer Audience</h1>
+            <div>
+                <label>
+                    Total Spend  ={'>'}
+                    <input
+                        type="number"
+                        value={totalSpend}
+                        onChange={(e) => setTotalSpend(e.target.value)}
+                    />
+                </label>
+                <br></br>
+                <br></br>
+                <label>
+                    Max Visits  ={'>'}
+                    <input
+                        type="number"
+                        value={maxVisits}
+                        onChange={(e) => setMaxVisits(e.target.value)}
+                    />
+                </label>
+                <br></br>
+                <br></br>
+                <label>
+                    Not Visited In Last Months  ={'>'}
+                    <input
+                        type="number"
+                        value={notVisitedInMonths}
+                        onChange={(e) => setNotVisitedInMonths(e.target.value)}
+                    />
+                </label>
+                <br></br>
+                <br></br>
+                <br></br>
+                <button onClick={handleFilter}>Filter</button>
+            </div>
+            <br></br>
+            <h2>Filtered Customers</h2>
+            <ul>
+                {customers.map((customer) => (
+                    <li key={customer._id}>{customer.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
